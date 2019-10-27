d681418dc2062ac158cfb322c67687c98e1c0a0b
Natalie Galligan <nat.galligan@gmail.com>
2019-10-27 12:10:00 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,9 +1,17 @@
-def using_while
+# def using_while
+#   counter = 6
+# 	while counter < 10
+#     puts "Wingardium Leviosa"
+#     counter += 10
+#   end
+# end
+#
+# using_while
+
+
+
   counter = 6
 	while counter < 10
     puts "Wingardium Leviosa"
     counter += 10
   end
-end
-
-using_while
